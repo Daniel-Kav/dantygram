76abdb784dc5087f9eb7aa19cbdc455f18aada92
daniel-kav <dankavatha01@gmail.com>
2023-12-31 01:42:50 +0300

    tailwind css setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './globals.css';
+
+const app = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default app
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom';
+
+import  ReactDOM  from 'react-dom/client';
+import App from './app';
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <app />
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <App />
 )
