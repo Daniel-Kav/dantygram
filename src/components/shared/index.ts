c2c37920dea912129e4fa26c32148de19c01358a
daniel-kav <dankavatha01@gmail.com>
2024-01-12 21:46:34 +0300

    working on the postform to upload files

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,54 +1,10 @@
-import { Models } from "appwrite";
-import { Link } from "react-router-dom";
-
-import { PostStats } from "@/components/shared";
-import { useUserContext } from "@/context/AuthContext";
-
-type GridPostListProps = {
-  posts: Models.Document[];
-  showUser?: boolean;
-  showStats?: boolean;
-};
-
-const GridPostList = ({
-  posts,
-  showUser = true,
-  showStats = true,
-}: GridPostListProps) => {
-  const { user } = useUserContext();
-
-  return (
-    <ul className="grid-container">
-      {posts.map((post) => (
-        <li key={post.$id} className="relative min-w-80 h-80">
-          <Link to={`/posts/${post.$id}`} className="grid-post_link">
-            <img
-              src={post.imageUrl}
-              alt="post"
-              className="h-full w-full object-cover"
-            />
-          </Link>
-
-          <div className="grid-post_user">
-            {showUser && (
-              <div className="flex items-center justify-start gap-2 flex-1">
-                <img
-                  src={
-                    post.creator.imageUrl ||
-                    "/assets/icons/profile-placeholder.svg"
-                  }
-                  alt="creator"
-                  className="w-8 h-8 rounded-full"
-                />
-                <p className="line-clamp-1">{post.creator.name}</p>
-              </div>
-            )}
-            {showStats && <PostStats post={post} userId={user.id} />}
-          </div>
-        </li>
-      ))}
-    </ul>
-  );
-};
-
-export default GridPostList;
+export { default as Bottombar } from "./Bottombar";
+export { default as LeftSidebar } from "./LeftSidebar";
+export { default as Loader } from "./Loader";
+export { default as PostCard } from "./PostCard";
+export { default as GridPostList } from "./GridPostList";
+export { default as Topbar } from "./Topbar";
+export { default as UserCard } from "./UserCard";
+export { default as FileUploader } from "./FileUploader";
+export { default as ProfileUploader } from "./ProfileUploader";
+export { default as PostStats } from "./PostStats";
